7207661918217f5f63acc0f0a0cfe12573fd13bb
akshatkamboj <akshatkamboj37@gmail.com>
2024-11-15 21:35:19 +0530

    Initial Application Setup

---
+++ src/test/java/com/assignment/userprofileservice/UserprofileserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.userprofileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserprofileserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
